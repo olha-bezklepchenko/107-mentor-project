423c865e005fff2fc8bcf5be121a0e383b2ad511
Olha Bezklepchenko <krolichek.11@gmail.com>
2024-08-04 13:52:15 +0200

    add api

---
+++ src/js/unsplesh-api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'https://api.unsplash.com';
+async function getPhotos(query) {
+  const API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
+
+  try {
+    const response = await axios.get('/search/photos', {
+      params: {
+        query,
+        page: 1,
+        per_page: 12,
+        orientation: 'portrait',
+        client_id: API_KEY,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getPhotos('cat').then(res => console.log(res));
